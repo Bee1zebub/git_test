dbef6448901662499b727cd567bcc49c687b6bb4
CMY <137265690@qq.com>
2022-10-07 09:31:16 +0800

    fifth commit

--- src/main/java/org/example/GitTest.java
+++ src/main/java/org/example/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Git Test2");
         System.out.println("faf");
+        System.out.println("test4");
     }
 }
