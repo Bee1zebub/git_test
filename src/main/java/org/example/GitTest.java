ecc6bc8cd4381bc6cd586fb69d3b4b012135b0d5
CMY <137265690@qq.com>
2022-10-07 09:22:39 +0800

    fourth commit

--- src/main/java/org/example/GitTest.java
+++ src/main/java/org/example/GitTest.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("Git Test");
+        System.out.println("Git Test2");
         System.out.println("faf");
     }
 }
